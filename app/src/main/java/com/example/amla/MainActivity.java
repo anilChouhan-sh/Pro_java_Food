e3c637aee7c579d091769c4e3990a3a3faa97abe
laksh <lakshay88>
2020-05-15 23:28:07 +0530

    hello

--- app/src/main/java/com/example/amla/MainActivity.java
+++ app/src/main/java/com/example/amla/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.amla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public void signupclicked(View view) {
+        EditText username = findViewById(R.id.username);
+        EditText username = findViewById(R.id.username);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
