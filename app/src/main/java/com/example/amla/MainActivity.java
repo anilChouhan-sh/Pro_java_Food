5b98413cdee4e6ba9ba10de4c1dd82e466a3534b
flask <c.anil5141@gmail.com>
2020-05-16 15:35:14 +0530

    parse trial

--- app/src/main/java/com/example/amla/MainActivity.java
+++ app/src/main/java/com/example/amla/MainActivity.java
@@ -3,20 +3,58 @@ package com.example.amla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
     public void signupclicked(View view) {
-        EditText username = findViewById(R.id.username);
-        EditText username = findViewById(R.id.username);
+        TextView username = findViewById(R.id.username);
+        EditText usernamep = findViewById(R.id.userpassword);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("hello")
+                .clientKey("world")
+                .server("http://192.168.43.168:1338/parse")
+                .build()
+        );
+
+        ParseObject object = new ParseObject("ExampleObject");
+        object.put("myNumber", "123");
+        object.put("myString", "rob");
+
+        object.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException ex) {
+                if (ex == null) {
+                    Log.i("Parse Result", "Successful!");
+                } else {
+                    Log.i("Parse Result", "Failed" + ex.toString());
+                }
+            }
+        });
 
 
+        ParseUser.enableAutomaticUser();
+
+        ParseACL defaultACL = new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
     }
 }
