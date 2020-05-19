f1b3b3a45c911c88a988fdfabf336779531c807b
flask <c.anil5141@gmail.com>
2020-05-19 23:48:22 +0530

    address clickable

--- app/src/main/java/com/example/amla/MyProfile.java
+++ app/src/main/java/com/example/amla/MyProfile.java
@@ -3,58 +3,21 @@ package com.example.amla;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MyProfile#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MyProfile extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class MyProfile extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public MyProfile() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyProfile.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyProfile newInstance(String param1, String param2) {
-        MyProfile fragment = new MyProfile();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        LinearLayout adress_layout ;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,7 +26,16 @@ public class MyProfile extends Fragment {
         return inflater.inflate(R.layout.fragment_my_profile, container, false);
     }
 
-    public void clicked_on_address(View view) {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+            adress_layout = view.findViewById(R.id.linearLayout_2);
+            adress_layout.setOnClickListener(this);
+    }
+
+
+   @Override
+    public void onClick(View v) {
         Intent intent = new Intent(getContext(), User_address.class);
         startActivity(intent);
     }
