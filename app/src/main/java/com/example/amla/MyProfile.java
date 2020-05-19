992f1723b332129943ec7621b2508575f8714573
flask <c.anil5141@gmail.com>
2020-05-20 00:12:13 +0530

    past order

--- app/src/main/java/com/example/amla/MyProfile.java
+++ app/src/main/java/com/example/amla/MyProfile.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,7 @@ import android.widget.LinearLayout;
 public class MyProfile extends Fragment implements View.OnClickListener {
 
 
-        LinearLayout adress_layout ;
+        LinearLayout address_layout  ,logout_layout  ,past_order;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,14 +30,40 @@ public class MyProfile extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-            adress_layout = view.findViewById(R.id.linearLayout_2);
-            adress_layout.setOnClickListener(this);
+
+            address_layout = view.findViewById(R.id.linearLayout_2);
+            logout_layout =view.findViewById(R.id.linearLayout_logout);
+            past_order =view.findViewById(R.id.linearLayout_1);
+
+
+            past_order.setOnClickListener(this);
+            address_layout.setOnClickListener(this);
+            logout_layout.setOnClickListener(this);
     }
 
 
    @Override
     public void onClick(View v) {
-        Intent intent = new Intent(getContext(), User_address.class);
-        startActivity(intent);
+
+
+        if (v.getId() == R.id.linearLayout_2)
+        {
+            Intent intent = new Intent(getContext(), User_address.class);
+            startActivity(intent);
+            //adress
+        }
+
+       if (v.getId() == R.id.linearLayout_logout)
+       {
+
+           //logout
+       }
+
+       if (v.getId() == R.id.linearLayout_1)
+       {
+
+           //pastorder
+       }
+
     }
 }
