07ab7144003d658ddaed10c8e26ff729ef6f1357
laksh <lakshay88>
2020-05-19 15:57:32 +0530

    change

--- app/src/main/java/com/example/amla/Restaurant_Info.java
+++ app/src/main/java/com/example/amla/Restaurant_Info.java
@@ -26,6 +26,8 @@ import java.util.Queue;
 public class Restaurant_Info extends AppCompatActivity {
 
     LinearLayout linlayout;
+
+    ImageView imageView =findViewById(R.id.imageView2);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,19 +45,18 @@ public class Restaurant_Info extends AppCompatActivity {
             public void done(List<ParseObject> objects, ParseException e) {
                 if(e==null){
                     for(ParseObject object : objects){
-
+                        Log.i("info", "image found!");
                         ParseFile file = (ParseFile) object.get("image");
                         file.getDataInBackground(new GetDataCallback() {
                             @Override
                             public void done(byte[] data, ParseException e) {
                                 if (e == null ) {
-                                    Log.i("info", "image found!");
+
                                     Bitmap bitmapImage = BitmapFactory.decodeByteArray(data, 0, data.length);
-                                    ImageView image = new ImageView(getApplicationContext());
-                                    imageView.
-                                    image.setImageBitmap(bitmapImage);
+
+                                    imageView.setImageBitmap(bitmapImage);
                                     //image.setImageDrawable(getResources().getDrawable(R.drawable.pic));
-                                    linlayout.addView(image);
+                                    linlayout.addView(imageView);
                                 } else {
                                     Log.i("info", e.getMessage());
                                 }
@@ -67,7 +68,7 @@ public class Restaurant_Info extends AppCompatActivity {
                 }
             }
         });
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Food");
+        /*ParseQuery<ParseObject> query = ParseQuery.getQuery("Food");
         query.addAscendingOrder("Food");
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
@@ -77,6 +78,29 @@ public class Restaurant_Info extends AppCompatActivity {
                             Log.i("dish", object.getString("Name"));
                             try {
                                // resto_name.add(object.getString("Resto_name"));
+                            }catch (Exception ex){
+                                //ex.printStackTrace();
+                            }
+                        }
+                    }
+
+                    //listView.setAdapter(arrayAdapter);
+                }
+                ////chnged
+                else {Log.i("parse exception " , e.getMessage());
+                }
+            }
+        });*/
+        ParseQuery<ParseObject> query2 = ParseQuery.getQuery("Restaurant");
+        query2.whereEqualTo("Resto_owner",resto_name);
+        query2.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    if(objects.size()>0) {
+                        for(ParseObject object : objects) {
+                            Log.i("owner", object.getString("Resto_owner"));
+                            try {
+                                // resto_name.add(object.getString("Resto_name"));
                             }catch (Exception ex){
                                 //ex.printStackTrace();
                             }
